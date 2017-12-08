66ad8f0655e59b1a9e2f68f3755563f270ae33e5
Kale Kundert <kale@thekunderts.net>
2017-12-08 00:55:58 -0800

    Document the config system a bit.

--- exmemo/commands/config.py
+++ exmemo/commands/config.py
@@ -10,6 +10,19 @@ def config():
 
     Usage:
         exmemo config
+
+    Each project reads configuration information from three different files.  
+    Typical paths for a Linux system are given below:
+
+        In-project: /path/to/project/.exmemorc
+        User-wide: ~/.config/exmemo/conf.toml
+        Site-wide: /etc/xdg/exmemo/conf.toml
+
+    Values in the earlier files take precedence over those in later files.  All 
+    three files are TOML (https://github.com/toml-lang/toml).  As of yet there 
+    is no definitive list of every configuration option understood by exmemo, 
+    but typically the help message for each command will mention any relevant 
+    configuration options.
     """
     args = cli.parse_args_via_docopt()
     work = Workspace.from_cwd()
