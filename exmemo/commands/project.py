2345d650cb0db486875f0e2c8f011f6dc4ee9374
Kale Kundert <kale@thekunderts.net>
2017-12-10 11:23:54 -0800

    Document the initial directory layout.

--- exmemo/commands/project.py
+++ exmemo/commands/project.py
@@ -38,6 +38,34 @@ def init():
 
             Note that you have to quote the title, otherwise the shell will 
             interpret it as multiple arguments.
+
+    This command will create the following directory structure:
+
+    analysis/
+       A python package containing code used for data analysis.  By virtue of 
+       being a package, this code can be run from anywhere in the project.  
+       You'll need to install it before you can use it:
+
+          $ pip install -e analysis
+
+    data/
+       A single directory for any data file you collect.  Data files that are 
+       relevant to a particular experiment can be symlinked into the notebook 
+       directory using `exmemo data link`.  By  default, this directory is 
+       ignored by git.
+
+    documents/
+       A directory for presentations, papers, and the like.  By default, this 
+       directory is ignored by git.
+
+    notebook/
+       A Sphinx directory with separate directories for each experiment you 
+       carry out.  An experiment is defined by asking and answering a single 
+       question, which may take a single day or several months.
+
+    protocols/
+       A directory for protocols, which can be text files, python script, Excel 
+       files, Word documents, whatever.
             
     Exmemo is fairly opinionated about which files to create and what to call 
     them, but for the most part these opinions are arbitrary and you can change 
