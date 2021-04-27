7a76f6a8e4b8d640ba21785a514951713bcddce4
Kale Kundert <kale@thekunderts.net>
2021-04-27 11:17:41 -0400

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as file:
 
 setup(
     name='exmemo',
-    version='0.2.0',
+    version='0.3.0',
     author='Kale Kundert',
     author_email='kale@thekunderts.net',
     url='https://github.com/kalekundert/exmemo',
